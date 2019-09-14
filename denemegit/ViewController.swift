2d068796d67c1363a893b17c479950eab27d729f
ERDEM DEMİRTAŞ <erdemdemirts@hotmail.com>
2019-09-14 14:13:39 +0300

    Initial Commit

---
+++ denemegit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  denemegit
+//
+//  Created by ERDEM DEMİRTAŞ on 14.09.2019.
+//  Copyright © 2019 ERDEM DEMİRTAŞ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
