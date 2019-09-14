cc7562bc483cb772cc03f65df57b7895cff9ba43
ERDEM DEMİRTAŞ <erdemdemirts@hotmail.com>
2019-09-14 14:30:59 +0300

    new File Commid

---
+++ denemegit/asd.swift
@@ -0,0 +1,10 @@
+//
+//  asd.swift
+//  denemegit
+//
+//  Created by ERDEM DEMİRTAŞ on 14.09.2019.
+//  Copyright © 2019 ERDEM DEMİRTAŞ. All rights reserved.
+//
+
+import Foundation
+print("second Commit")
